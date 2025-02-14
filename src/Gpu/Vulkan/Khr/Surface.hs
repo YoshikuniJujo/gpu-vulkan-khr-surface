96093a3de0b393a3b18cbc36e3127906c78440aa
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-02-14 17:34:39 +0900

    first commit

---
+++ src/Gpu/Vulkan/Khr/Surface.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Khr.Surface (
+	S, group, Group, unsafeDestroy, lookup,
+
+	Capabilities(..), Format(..),
+
+	-- * ENUM
+
+	module Gpu.Vulkan.Khr.Surface.Enum
+
+	) where
+
+import Prelude hiding (lookup)
+import Gpu.Vulkan.Khr.Surface.Internal
+import Gpu.Vulkan.Khr.Surface.Enum
